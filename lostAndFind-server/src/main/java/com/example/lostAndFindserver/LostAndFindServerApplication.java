8169dfb0806dc2fb7b187923757183c7e3c4b322
Mishan Madhupa <mishancool2016@gmail.com>
2020-05-03 14:11:20 +0530

    Start spring boot server and connect it to database

---
+++ lostAndFind-server/src/main/java/com/example/lostAndFindserver/LostAndFindServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lostAndFindserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFindServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAndFindServerApplication.class, args);
+	}
+
+}
