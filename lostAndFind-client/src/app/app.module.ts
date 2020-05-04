4885aa44d0eca7a883ea1b848d460a31c0ce0afc
Mishan Madhupa <mishancool2016@gmail.com>
2020-05-04 12:38:59 +0530

    Create navigation bar

--- lostAndFind-client/src/app/app.module.ts
+++ lostAndFind-client/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// components
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
