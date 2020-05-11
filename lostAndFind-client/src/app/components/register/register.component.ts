f01120121a59fd3d2dac63192803312c5cb3ff43
Mishan Madhupa <mishancool2016@gmail.com>
2020-05-11 23:59:32 +0530

    Error corection

--- lostAndFind-client/src/app/components/register/register.component.ts
+++ lostAndFind-client/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgFlashMessageService } from 'ng-flash-messages';
+import { Router } from '@angular/router';
 
 import { User } from './../../models/user.model';
 import { AuthenticationService } from './../../services/authentication.service';
@@ -10,15 +12,55 @@ import { AuthenticationService } from './../../services/authentication.service';
 })
 export class RegisterComponent implements OnInit {
 
+  username: string;
+  email: string;
+  password: string;
+  alert: { type: string; msg: any; };
+  success: boolean;
+
   constructor(
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private ngFlashMessageService: NgFlashMessageService,
+    private router: Router,
   ) { }
 
   ngOnInit() {
   }
 
   onRegisterSubmit() {
-    // this.authService.registerUser
+    const user = {
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    };
+
+    // register user
+    this.authService.registerUser(user).subscribe( (res) => {
+      if (res['success']) {
+        this.ngFlashMessageService.showFlashMessage({
+          messages: ['You are now registered'],
+          dismissible: true,
+          timeout: 3000,
+          type: 'success'
+        });
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        },
+        3000);
+
+      } else {
+        this.ngFlashMessageService.showFlashMessage({
+          messages: ['Registration failed'],
+          dismissible: true,
+          timeout: 3000,
+          type: 'danger'
+        });
+        setTimeout(() => {
+          this.router.navigate(['/register']);
+        },
+        3000);
+      }
+    });
   }
 
 }
