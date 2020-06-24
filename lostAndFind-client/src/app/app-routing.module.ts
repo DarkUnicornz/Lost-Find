922feeeb1b7487008f9a0bcb7a5814fb2c3c3b64
Mishan Madhupa <mishancool2016@gmail.com>
2020-06-24 23:58:23 +0530

    Create routes for admin-new and mod-new components

--- lostAndFind-client/src/app/app-routing.module.ts
+++ lostAndFind-client/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AdminNewComponent } from './pages/admin-new/admin-new.component';
+import { ModNewComponent } from './pages/mod-new/mod-new.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'new_admin', component: AdminNewComponent },
+  { path: 'new_mod', component: ModNewComponent },
 ];
 
 @NgModule({
