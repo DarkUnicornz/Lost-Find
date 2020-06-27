042d4271fb18f2419acaf70cf4213cbe18d132bb
Asiri Senith <asenith79@gmail.com>
2020-06-27 23:54:52 +0530

    Started creating Home Page UI

---
+++ lost_and_find_mobile/lib/screens/home/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Lost&Found_App'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
