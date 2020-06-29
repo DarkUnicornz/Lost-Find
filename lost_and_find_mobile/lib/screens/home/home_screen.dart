d9c4274a52862e03c60565f5cfe8f5354149587e
Asiri Senith <asenith79@gmail.com>
2020-06-29 15:07:11 +0530

    Started create complain UI

--- lost_and_find_mobile/lib/screens/home/home_screen.dart
+++ lost_and_find_mobile/lib/screens/home/home_screen.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
+
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
