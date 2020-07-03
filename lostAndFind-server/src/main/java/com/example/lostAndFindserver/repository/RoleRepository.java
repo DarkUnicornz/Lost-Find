57443aa5b1ed70ee12806725671617c1879989e8
Mishan Madhupa <mishancool2016@gmail.com>
2020-07-03 08:42:51 +0530

    Done role and user repositories

---
+++ lostAndFind-server/src/main/java/com/example/lostAndFindserver/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.example.lostAndFindserver.repository;
+
+import com.example.lostAndFindserver.model.ERole;
+import com.example.lostAndFindserver.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
+
