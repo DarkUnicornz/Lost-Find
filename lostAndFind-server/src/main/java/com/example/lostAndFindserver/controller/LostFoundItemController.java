af31b20683c0a57fa7ea3a97f74f714799de8145
Asiri Senith <asirisenith@gmail.com>
2020-07-15 23:25:10 +0530

    Edited LostFoundItemController function

--- lostAndFind-server/src/main/java/com/example/lostAndFindserver/controller/LostFoundItemController.java
+++ lostAndFind-server/src/main/java/com/example/lostAndFindserver/controller/LostFoundItemController.java
@@ -1,40 +1,46 @@
-// package com.example.lostAndFindserver.controller;
+ package com.example.lostAndFindserver.controller;
 
-// import com.example.lostAndFindserver.model.LostFoundItemModel;
+ import com.example.lostAndFindserver.model.LostFoundItemModel;
 // import com.example.lostAndFindserver.model.UserModel;
-// import com.example.lostAndFindserver.service.LostFoundItemService;
+ import com.example.lostAndFindserver.service.LostFoundItemService;
 // import com.example.lostAndFindserver.service.UserService;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.RequestBody;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RestController;
 
-// @RequestMapping("/paml")
+ @RequestMapping("/lostfoundItem")
 // @CrossOrigin(origins = "http://localhost:4200")
-// @RestController
-// public class LostFoundItemController {
+ @RestController
+ public class LostFoundItemController {
 
-//    @Autowired
-//    private LostFoundItemService lostFoundItemService;
+    @Autowired
+    private LostFoundItemService lostFoundItemService;
 //    private UserService userService;
 
-//    @PostMapping("/post")
+    @PostMapping("/post")
 //    @CrossOrigin(origins = "http://localhost:4200")
-//    public LostFoundItemModel savePost(@RequestBody LostFoundItemModel lostFoundItemModel) throws Exception {
+    public String savePost(@RequestBody LostFoundItemModel post) throws Exception {
 
 //        String tempNIC = lostFoundItemModel.getNic();
 //        if(tempNIC != null && "".equals(tempNIC)){
 //            UserModel userObj = userService.fetchUserByNIC(tempNIC);
-
+//
 //            if(userObj!=null){
 //                throw new Exception("You should register first");
 //            }
 //        }
-//        LostFoundItemModel lItemObj = null;
-//        lItemObj = lostFoundItemService.savePost(lostFoundItemModel);
-//        return lItemObj;
-//    }
-// }
+//        String lItemObj = null;
+        LostFoundItemModel lostFoundItemModel = new LostFoundItemModel(
+                post.getItemId(),
+                post.getDescription(),
+                post.getLocation()
+        );
+
+        return lostFoundItemService.savePost(lostFoundItemModel);
+        //return lItemObj;
+    }
+ }
--- lostAndFind-server/src/main/java/com/example/lostAndFindserver/service/LostFoundItemService.java
+++ lostAndFind-server/src/main/java/com/example/lostAndFindserver/service/LostFoundItemService.java
@@ -1,18 +1,19 @@
-//package com.example.lostAndFindserver.service;
-//
-//import com.example.lostAndFindserver.model.LostFoundItemModel;
-//import com.example.lostAndFindserver.repository.LostFoundItemRepository;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class LostFoundItemService {
-//
-//    @Autowired
-//    private LostFoundItemRepository lostFoundItemRepository;
-//
-//    public LostFoundItemModel savePost(LostFoundItemModel lostFoundItemModel){
-//        return lostFoundItemRepository.save(lostFoundItemModel);
-//    }
-//}
+package com.example.lostAndFindserver.service;
+
+import com.example.lostAndFindserver.model.LostFoundItemModel;
+import com.example.lostAndFindserver.repository.LostFoundItemRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LostFoundItemService {
+
+    @Autowired
+    private LostFoundItemRepository lostFoundItemRepository;
+
+    public String savePost(LostFoundItemModel lostFoundItemModel){
+        lostFoundItemRepository.save(lostFoundItemModel);
+        return "Success";
+    }
+}
