8638eb3854f2fd2114d66d2d9dc52157eacc4f13
Mishan Madhupa <mishancool2016@gmail.com>
2020-09-03 16:51:57 +0530

    Fix error lostfounditem controller

--- lostAndFind-server/src/main/java/com/example/lostAndFindserver/controller/LostFoundItemController.java
+++ lostAndFind-server/src/main/java/com/example/lostAndFindserver/controller/LostFoundItemController.java
@@ -1,52 +1,34 @@
-//package com.example.lostAndFindserver.controller;
-//
-//import com.example.lostAndFindserver.model.LostFoundItemModel;
-//import com.example.lostAndFindserver.payload.request.PostRequest;
-//import com.example.lostAndFindserver.payload.response.MessageResponse;
-//import com.example.lostAndFindserver.repository.LostFoundItemRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.validation.Valid;
-//
-//@CrossOrigin(origins = "*", maxAge = 3600)
-//@RestController
-//@RequestMapping("/lostfoundItem")
-//public class LostFoundItemController {
-//
-//    // @Autowired
-//    // private LostFoundItemService lostFoundItemService;
-//
-//    @Autowired
-//    PostRequest postRequest;
-//
-//    @Autowired
-//    LostFoundItemRepository lostfoundItemRepository;
-//
-//    @PostMapping("/post")
-//    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-//    public ResponseEntity<?> savePost(@Valid @RequestBody PostRequest postrequest) throws Exception {
-//
-////        String tempNIC = lostFoundItemModel.getNic();
-////        if(tempNIC != null && "".equals(tempNIC)){
-////            lostFoundItemModel lostFoundObj = LostFoundItemService.fetchUserByNIC(tempNIC);
-////
-////            if(userObj!=null){
-////                throw new Exception("You should register first");
-////            }
-////        }
-////        String lItemObj = null;
-//        LostFoundItemModel lostFoundItemModel = new LostFoundItemModel(
-//                postrequest.getItemId(),
-//                postrequest.getDescription(),
-//                postrequest.getLocation()
-//            );
-//
-//        lostfoundItemRepository.save(lostFoundItemModel);
-//
-//        return ResponseEntity.ok(new MessageResponse("Post successfully!"));
-//    }
-//
-//}
+package com.example.lostAndFindserver.controller;
+
+import com.example.lostAndFindserver.model.LostFoundItemModel;
+import com.example.lostAndFindserver.payload.request.PostRequest;
+import com.example.lostAndFindserver.service.LostFoundItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/lost&founditem")
+public class LostFoundItemController {
+
+    @Autowired
+    private LostFoundItemService lostFoundItemService;
+
+    @PostMapping("/post")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public LostFoundItemModel savePost(@Valid @RequestBody PostRequest postrequest){
+
+        LostFoundItemModel lostFoundItemModel = new LostFoundItemModel(
+                postrequest.getDescription(),
+                postrequest.getLocation());
+
+        return lostFoundItemService.savePost(lostFoundItemModel);
+
+//        LostFoundItemRepository.save(lostFoundItemModel);
+//        return ResponseEntity.ok(new MessageResponse("Create post successfully!"));
+    }
+
+}
---
+++ lostAndFind-server/src/main/java/com/example/lostAndFindserver/controller/UserController.java
@@ -0,0 +1,34 @@
+//package com.example.lostAndFindserver.controller;
+//
+//
+//import com.example.lostAndFindserver.model.LostFoundItemModel;
+//import com.example.lostAndFindserver.payload.request.PostRequest;
+//import com.example.lostAndFindserver.service.LostFoundItemService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.validation.Valid;
+//
+//@CrossOrigin(origins = "*", maxAge = 3600)
+//@RestController
+//@RequestMapping("/api/user")
+//public class UserController {
+//
+//    @Autowired
+//    private LostFoundItemService lostFoundItemService;
+//
+//    @PostMapping("/post")
+//    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+//    public LostFoundItemModel savePost(@Valid @RequestBody PostRequest postrequest){
+//
+//        LostFoundItemModel lostFoundItemModel = new LostFoundItemModel(
+//                postrequest.getDescription(),
+//                postrequest.getLocation());
+//
+//        return lostFoundItemService.savePost(lostFoundItemModel);
+//
+////        LostFoundItemRepository.save(lostFoundItemModel);
+////        return ResponseEntity.ok(new MessageResponse("Create post successfully!"));
+//    }
+//}
