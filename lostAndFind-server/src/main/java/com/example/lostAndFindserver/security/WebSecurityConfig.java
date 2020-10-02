f020c9b26b69c7464e8b61a176bbee818f2d322e
Mishan Madhupa <mishancool2016@gmail.com>
2020-10-02 19:26:39 +0530

    Git add dashboard template

--- lostAndFind-server/src/main/java/com/example/lostAndFindserver/security/WebSecurityConfig.java
+++ lostAndFind-server/src/main/java/com/example/lostAndFindserver/security/WebSecurityConfig.java
@@ -62,6 +62,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
                 .antMatchers("/api/lost&founditem/**").permitAll()
+                .antMatchers("/api/user/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
