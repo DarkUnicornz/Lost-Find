7c7bce01fe20e04702ac6a7cf273c39cc3d96063
Mishan Madhupa <mishancool2016@gmail.com>
2020-10-08 00:55:29 +0530

    View user complains back end

--- lostAndFind-server/src/main/java/com/example/lostAndFindserver/service/ComplainService.java
+++ lostAndFind-server/src/main/java/com/example/lostAndFindserver/service/ComplainService.java
@@ -1,9 +1,13 @@
 package com.example.lostAndFindserver.service;
 
 import com.example.lostAndFindserver.model.Complain;
+import com.example.lostAndFindserver.model.User;
 import com.example.lostAndFindserver.repository.ComplainRepository;
+//import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ComplainService {
@@ -13,5 +17,9 @@ public class ComplainService {
 
     public Complain saveComplain(Complain complain) {
         return complainRepository.save(complain);
+    }
+
+    public List<Complain> getUserComplain(User u_id) {
+        return complainRepository.findByUser(u_id);
     }
 }
